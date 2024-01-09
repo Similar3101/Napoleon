9c28a73cef9bdd328e0acfc899af5ef3630e7160
murtiuwu <152479678+murtiuwu@users.noreply.github.com>
2024-01-09 20:55:31 +0800

    this is useless

--- Napoleon_code.py
+++ Napoleon_code.py
@@ -2,26 +2,17 @@ import string
 
 def napoleon_code(text, move_num=0):
     result_list = []
-    end_of_while = True
     alphovite = string.ascii_lowercase
     for letter in text:
         letter = letter.lower()
         if letter in alphovite:
             letter_index = alphovite.index(letter)
-
-            while end_of_while:
-                try:
-                    end_of_while = False
-                    result_list.insert(0, alphovite[abs(letter_index + move_num)])
-                except:
-                    move_num -= len(alphovite)
-                    end_of_while = True
-            end_of_while = True
+            result_list.append(alphovite[abs(letter_index + (move_num % len(alphovite)))])
         else:
             print(letter, end='')
-    result_list.reverse()
-    print(result_list)
+    print(''.join(result_list))
+    print(len(alphovite))
 
-napoleon_code('hallo', 30)
+napoleon_code('hallo', 82)
 
 print('end')
